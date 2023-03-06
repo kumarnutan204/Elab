1b17110a7c00906417a288384202ef829cb7445f
kumarnutan204 <74036964+kumarnutan204@users.noreply.github.com>
2023-03-07 00:57:23 +0530

    Dhoni's daughter Ziva is a hyperactive

---
+++ Dhoni's daughter.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{int weightinearth;
+float weightinmoon;
+cin>>weightinearth;
+weightinmoon=16.6*weightinearth/100;
+cout<<weightinmoon;
+	return 0;
+}
