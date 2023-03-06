a9266d8238d0369e7e8d02c990ab6cbc2ac38403
kumarnutan204 <74036964+kumarnutan204@users.noreply.github.com>
2023-03-07 01:12:05 +0530

    Johit and Rohit are classmates who often challenge

---
+++ OOPS/Johit and Rohit.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class HiddenTime{
+    public:
+    void maximumTime(string time) {
+        if(time[0]=='?'){
+            time[0]=(time[1]<='3' || time[1]=='?')?'2':'1';
+}
+if(time[1]=='?'){
+    time[1]=(time[0]=='2')?'3':'9';
+}
+if(time[3]=='?'){
+    time[3]='5';
+}
+if(time[4]=='?'){
+    time[4]='9';
+}
+cout<<time;
+}
+};
+int main(){
+    string time;
+    cin>>time;
+    HiddenTime LatestTime;
+    LatestTime.maximumTime(time);return 0;}
