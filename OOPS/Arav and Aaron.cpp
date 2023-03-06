cdb8b008b1c20be3f8aa705c64484ec3aefd5af9
kumarnutan204 <74036964+kumarnutan204@users.noreply.github.com>
2023-03-07 01:03:37 +0530

    Arav and Aaron are participating in the bike

---
+++ OOPS/Arav and Aaron.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{int aravspeed,aaronspeed,speeddiff;
+cin>>aravspeed>>aaronspeed;
+if(aravspeed>aaronspeed)
+speeddiff=aravspeed - aaronspeed;
+else
+speeddiff= aaronspeed- aravspeed;
+cout<<speeddiff;
+
+	return 0;
+}
