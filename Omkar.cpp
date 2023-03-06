e87c97cdf711c3a8d9f7f8dd8ae5dfeb59dbb6d6
kumarnutan204 <74036964+kumarnutan204@users.noreply.github.com>
2023-03-07 00:07:41 +0530

    Omkar the professional

---
+++ Omkar.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{int M,initialtemp,finaltemp;
+cin>>M>>initialtemp>>finaltemp;
+float Q;
+Q=M*(finaltemp - initialtemp)*4184;
+cout<<""<<Q;
+	return 0;
+}
