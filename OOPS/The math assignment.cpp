0b25342eb1863f5b457f0bc3cb4eef01d4f0cca4
kumarnutan204 <74036964+kumarnutan204@users.noreply.github.com>
2023-03-09 00:05:39 +0530

    The math assignment of the weekend is that

---
+++ OOPS/The math assignment.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class complex
+{
+    private:
+      float real;
+      float imag;
+    public:
+       complex() {cin>>real>>imag;}
+       complex operator-(complex ob)
+       {
+           complex t;
+           t.real = real - ob.real;
+           t.imag = imag - ob.imag;
+           return t;
+       }
+
+       void output()
+       {
+           if(imag < 0)
+               cout<< real << imag << "i"<<endl;
+           else
+               cout<< real << "+" << imag << "i"<<endl;
+       }
+};
+int main()
+{
+    complex c1, c2;
+    c1.output();
+    c2.output();
+    (c1 - c2).output();
+    return 0;
+}
