0ecf2b0054471edac31d90757f912e3e42c94411
kumarnutan204 <74036964+kumarnutan204@users.noreply.github.com>
2023-03-09 00:11:24 +0530

    Ravi is a higher secondary school student.

---
+++ OOPS/Ravi is a higher secondary.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void display(){
+    
+    cout<<"friend void  operator >> in >> class Cutoff ";
+    }
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    
+    cout<<(int)(a+0.5*b+0.5*c);
+	return 0;
+}
